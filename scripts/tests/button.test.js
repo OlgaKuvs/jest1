090071f199d460b80e4ff12d36cff2b7264886ed
OlgaKuvs <olja_kuv@inbox.lv>
2023-08-02 18:51:04 +0100

    Initial commit

---
+++ scripts/tests/button.test.js
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const buttonClick = require("../button");
+
+beforeEach ( () => {
+    // document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs"); // Node module to open and read files
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+
+});
+
+describe("DOM test", () => {
+    test("expects p content to change", () => {
+        buttonClick();
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+
+    test("h1 should exist", () => {        
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    });
+});
